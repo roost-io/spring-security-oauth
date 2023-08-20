f3773f8d6d6761c7e000ee94fcb5d5faae7beec9
anuragkumawat <anuragkumawat7@gmail.com>
2023-08-20 14:04:02 +0530

    JAVA-24275 Upgrade Keycloak for oauth-resource-server/authorization-server

--- oauth-resource-server/authorization-server/src/main/java/com/baeldung/authserver/config/Resteasy3Provider.java
+++ oauth-resource-server/authorization-server/src/main/java/com/baeldung/authserver/config/Resteasy3Provider.java
@@ -1,6 +1,7 @@
 package com.baeldung.authserver.config;
 
-import org.jboss.resteasy.core.Dispatcher;
+import org.jboss.resteasy.core.ResteasyContext;
+import org.jboss.resteasy.spi.Dispatcher;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.common.util.ResteasyProvider;
 
@@ -8,28 +9,26 @@ public class Resteasy3Provider implements ResteasyProvider {
 
     @Override
     public <R> R getContextData(Class<R> type) {
-        ResteasyProviderFactory.getInstance();
-        return ResteasyProviderFactory.getContextData(type);
+        return ResteasyProviderFactory.getInstance()
+            .getContextData(type);
     }
 
     @Override
     public void pushDefaultContextObject(Class type, Object instance) {
-        ResteasyProviderFactory.getInstance();
-        ResteasyProviderFactory.getContextData(Dispatcher.class)
+        ResteasyProviderFactory.getInstance()
+            .getContextData(Dispatcher.class)
             .getDefaultContextObjects()
             .put(type, instance);
     }
 
     @Override
     public void pushContext(Class type, Object instance) {
-        ResteasyProviderFactory.getInstance();
-        ResteasyProviderFactory.pushContext(type, instance);
+        ResteasyContext.pushContext(type, instance);
     }
 
     @Override
     public void clearContextData() {
-        ResteasyProviderFactory.getInstance();
-        ResteasyProviderFactory.clearContextData();
+        ResteasyContext.clearContextData();
     }
 
 }
