87be61b62292a13fc3f23564991dc4ead0aac031
Forb Yuan <f0rb@163.com>
2024-01-28 16:25:33 +0800

    [BAEL-6472] Update article https://www.baeldung.com/spring-security-oauth-auth-server

--- oauth-authorization-server/client-server/src/main/java/com/baeldung/web/ArticlesController.java
+++ oauth-authorization-server/client-server/src/main/java/com/baeldung/web/ArticlesController.java
@@ -1,20 +1,20 @@
 package com.baeldung.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import javax.servlet.http.HttpServletRequest;
-
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @RestController
 public class ArticlesController {
 
-    @Autowired
+    public ArticlesController(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
     private WebClient webClient;
 
     @GetMapping(value = "/articles")
